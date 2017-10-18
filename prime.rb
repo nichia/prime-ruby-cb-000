36d8f72c77a5eedc1cc44e0279a05afcfdcca920
Ni Chia <nichia@gmail.com>
2017-10-18 17:17:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+require 'pry'
+
+def prime?(number)
+  is_prime = false
+  
+  if number > 1
+    is_prime = true
+    divisor = 2
+    until (divisor > number) || (is_prime == false)
+      if (number % divisor == 0)
+        is_prime = false
+      end
+      divisor = divisor + 1
+      binding.pry
+    end
+  end
+  
+  return is_prime
+end
+
+
