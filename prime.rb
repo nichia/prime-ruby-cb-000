aa38bc1c0a4ecce3efbeb5b33bcc6fa5d2e27ef6
Ni Chia <nichia@gmail.com>
2017-10-18 18:11:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,7 @@ def prime?(number)
   if number > 1
     is_prime = true
     divisor = 2
-    until (divisor > number) || (is_prime == false)
+    until (divisor >= number) || (is_prime == false)
       if (number % divisor == 0)
         is_prime = false
       end
